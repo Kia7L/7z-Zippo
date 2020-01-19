f545cddd786de902e81a9a823beea646dc925f3e
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-01-18 15:19:13 -1000

    Fixed

---
+++ 7z-zipo.py
@@ -0,0 +1,66 @@
+import webbrowser
+import os
+import colorama
+import tkinter
+from os import *
+from subprocess import call
+from time import sleep
+from tkinter import *
+from colorama import *
+#top-
+os.system("Title L-Zipo")
+os.system("color 01")
+os.system("mode con: cols=90 lines=5")
+cls = ('cls')
+os.system(cls)
+#--------------------------------
+#Variables-
+file0=('cecho.exe');
+file1=('7z.exe')
+#--------------------------------
+#echo-
+echoFirst = (file0+' {01}L-zipo.py{94}ByKia7L{\n}')
+echoSecond = (file0+' {06}ChooseFolder USED .\{70}{08}{\n}')
+os.system(echoFirst)
+print("")
+os.system(echoSecond)
+print("")
+#Command-
+Compression=(raw_input('FOLDER_COMPRESS_LOCATION|.File_Type: '))
+Saved=(raw_input('FILE_NAME_SAVED|.File_Type-iso,7z,gzip,zip2,zip.100: '))
+#-------------------------------------------------------------
+Formula=('-9*11111111/0.00000001/0x01')
+cover=(' "./' )
+one=(file1+' a')
+two=(' -ax'+Formula)
+three=(cover+Saved)
+four=('" "'+Compression)
+cap=('"')
+print(one+two+three+four+cap)
+echoThird = (file0+' {04}4{03}Continue...?{05}OK...?{\n}')
+os.system(echoThird)
+print("")
+sleep(4)
+os.system(one+two+three+four+cap) 
+print(one+two+three+four+cap)
+#--------------------------------
+color=(file0+' {94} {\n}')
+color2=(file0+' {74} {\n}')
+print("")
+os.system(color)
+FinalCode1=(raw_input('FinishUP|CODE: '))
+os.system(color2)
+os.system(FinalCode1)
+print(FinalCode1)
+#--------------------------------
+#--------------------------------
+color=(file0+' {94} {\n}')
+color2=(file0+' {74} {\n}')
+print("")
+os.system(color)
+FinalCode2=(raw_input('FinishLAST|CODE: '))
+os.system(color2)
+os.system(FinalCode2)
+print(FinalCode2)
+#--------------------------------
+input()
